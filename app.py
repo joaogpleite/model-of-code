99b68ef707537d562e7d0bba5d107290df49e2f4
João Gabriel Leite <114266021+joaogpleite@users.noreply.github.com>
2023-04-10 03:05:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,5 @@ def classify(message):
 def start(message):
     bot.send_message(message.chat.id, "Olá, para classificar a sua planilha digite classificar")
 
-@app.route("/classificar")
-def classificar():
-  sheet.append_row(["Licitações classificadas!])
-  return "Planilha escrita!"
-
-
 
 bot.polling()
